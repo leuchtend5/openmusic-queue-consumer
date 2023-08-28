dccbfba84ad33cb19e46450b8bcf1ffa3c8ba41d
leuchtend5 <zxcmj1@gmail.com>
2023-08-28 23:37:51 +0800

    bug fixed

--- src/PlaylistsService.js
+++ src/PlaylistsService.js
@@ -5,17 +5,24 @@ class PlaylistsService {
     this._pool = new Pool();
   }
 
-  async getSongsFromPlaylist(playlistId) {
+  async getSongs(playlistId) {
     const query = {
       text: `SELECT songs.id, songs.title, songs.performer FROM songs
-      JOIN playlist_songs ON songs.id = playlist_songs.song_id
+      LEFT JOIN playlist_songs ON songs.id = playlist_songs.song_id
       WHERE playlist_songs.playlist_id = $1`,
       values: [playlistId],
     };
-
     const result = await this._pool.query(query);
-
     return result.rows;
+  }
+
+  async getPlaylists(playlistId) {
+    const query = {
+      text: 'SELECT playlists.id,playlists.name FROM playlists WHERE playlists.id = $1',
+      values: [playlistId],
+    };
+    const result = await this._pool.query(query);
+    return result.rows[0];
   }
 }
 
--- src/listener.js
+++ src/listener.js
@@ -9,14 +9,19 @@ class Listener {
   async listen(message) {
     try {
       const { playlistId, targetEmail } = JSON.parse(message.content.toString());
-      const songFromPlaylist = this._playlistsService.getSongsFromPlaylist(playlistId);
-      console.log(songFromPlaylist);
 
-      const playlist = {
-        playlist: songFromPlaylist,
+      const songs = await this._playlistsService.getSongs(playlistId);
+      const playlists = await this._playlistsService.getPlaylists(playlistId);
+
+      const playlistSongs = {
+        playlist: {
+          id: playlistId,
+          name: playlists.name,
+          songs,
+        },
       };
 
-      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlist));
+      const result = await this._mailSender.sendEmail(targetEmail, JSON.stringify(playlistSongs));
 
       console.log(result);
     } catch (error) {
